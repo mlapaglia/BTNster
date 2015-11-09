6cbb5d4be051aaa2d20ea17eb23c03edb27b0e90
Matt LaPaglia <mlapaglia@gmail.com>
2015-11-09 09:00:06 -0500

    adding filter model

---
+++ BTNster.Models/Filters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTNster.Models
+{
+    public class Filter
+    {
+        public String FilterName { get; set; }
+
+        public List<String> Containers { get; set; }
+
+        public List<String> Sources { get; set; }
+
+        public List<String> Resolutions { get; set; }
+
+        public List<String> Codecs { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public Boolean? RememberHistory { get; set; }
+
+        public Boolean? DownloadBetterReleases { get; set; }
+
+        public Boolean? SceneYes { get; set; }
+
+        public Boolean? SceneNo { get; set; }
+    }
+
+    public class Series
+    {
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public int Season { get; set; }
+
+        public int Episode { get; set; }
+    }
+}
